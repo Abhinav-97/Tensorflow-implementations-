4861e9c650208ecb66a61c481fc0fb91864ab2dc
abhinav <abhinavdutt19@gmail.com>
2018-06-26 10:41:27 +0530

    ran conv-nets

--- rnn-net.py
+++ rnn-net.py
@@ -11,17 +11,17 @@ chunk_size = 28
 n_chunks = 28
 rnn_size = 128
 
-x = tf.placeholder("float", [None, n_chunks, chunk_size)])
+x = tf.placeholder("float", [None, n_chunks, chunk_size])
 y = tf.placeholder("float")
 
 
-def recurrent_nueral_network(data):
+def recurrent_nueral_network(x):
 
     layer = {"weights": tf.Variable(tf.random_normal([rnn_size, n_classes])),
                       "biases": tf.Variable(tf.random_normal([n_classes]))}
 
     x = tf.transpose(x, [1,0,2])
-    x = tf.reshape(-1, chunk_size)
+    x = tf.reshape(x, [-1, chunk_size])
     x = tf.split(x, chunk_size, 0)
 
     lstm_cell = rnn_cell.BasicLSTMCell(rnn_size, state_is_tuple=True)    
@@ -33,6 +33,7 @@ def recurrent_nueral_network(data):
 
 
 def train_recurrent_nueral_network(x):
+	print("Model Started")
 	predictions = recurrent_nueral_network(x)
 	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=predictions, labels=y))
 	optimizer = tf.train.AdamOptimizer().minimize(cost)
@@ -45,18 +46,18 @@ def train_recurrent_nueral_network(x):
 			epoch_loss = 0
 			for _ in range(int(mnist.train.num_examples/batch_size)):
 				epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-
-                epoch_x = epoch_x.reshape((batch_size, n_chunks, chunk_size))
+				epoch_x = epoch_x.reshape((batch_size, n_chunks, chunk_size))
+				# epoch_x = epoch_x.reshape((batch_size, n_chunks, chunk_size))
 				_, c = sess.run([optimizer, cost], feed_dict={ x: epoch_x, y: epoch_y })
 				epoch_loss += c
 
 			print("Epoch ", epoch, " completed out of ",hm_epochs, "loss: ", epoch_loss)
 
 	# evaluation of accuracy
-	correct = tf.equal(tf.argmax(predictions, 1), tf.argmax(y, 1))
-	accuracy = tf.reduce_mean(tf.cast(correct, "float"))
+		correct = tf.equal(tf.argmax(predictions, 1), tf.argmax(y, 1))
+		accuracy = tf.reduce_mean(tf.cast(correct, "float"))
 
-	print("Accuracy: ", accuracy.eval({x:mnist.test.images.reshape((-1, n_chunks, chunk_size)),y:mnist.test.labels}))
+		print("Accuracy: ", accuracy.eval({x:mnist.test.images.reshape((-1, n_chunks, chunk_size)),y:mnist.test.labels}))
 
 
 train_recurrent_nueral_network(x)
